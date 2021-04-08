398270742be9a8a2dc01e24177646675192b000d
rodolfoarguello <rodolfo@maskot.co>
2021-04-08 17:12:09 +1000

    asdf

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ app.post("/register", (req, res) => {
 
 app.get("/login", (req, res) => {
   if (req.session.user) {
-    res.send({ loggedIn: true, user: req.session.user });
+    res.send({ logasdfgedIn: true, user: req.session.user });
   } else {
     res.send({ loggedIn: false });
   }
